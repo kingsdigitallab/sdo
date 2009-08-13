2e141ffa236168298dd612e88b3cad4af254e19f
pvetch <pvetch@7437ebc0-4e4f-0410-a91c-e60b824a4b0e>
2009-08-13 13:19:14 +0000

    Replaced ppack components with more recent versions and revised styling
    git-svn-id: file:///project/repositories/svn2/schenker/trunk@107 7437ebc0-4e4f-0410-a91c-e60b824a4b0e

--- _a/s/mod_sifr_config.js
+++
@@ -1,5 +0,0 @@
-if(typeof sIFR == "function"){
-	
-sIFR.replaceElement(named({sSelector:"#mainContent .pageHeader .t01 h1", sFlashSrc:"../f/myriad-r.swf", sColor:"#ff3300", sLinkColor:"#b10736", sBgColor:"#ffffff", sHoverColor:"#b10736", nPaddingTop:0, nPaddingBottom:0, sFlashVars:"textalign=left&offsetTop=0&offsetLeft=0&offsetRight=0"}));
-sIFR.replaceElement(named({sSelector:"#mainContent .pageHeader .t01 h2", sFlashSrc:"../f/scala-r.swf", sColor:"#990000", sLinkColor:"#b10736", sBgColor:"#ffffff", sHoverColor:"#b10736", sCase: "upper", nPaddingTop:0, nPaddingBottom:0, sFlashVars:"textalign=left&offsetTop=0&offsetLeft=0&offsetRight=0"}));
-};
