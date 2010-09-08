85f8256ae53d7470678673334dbd0c14cd32c4b8
jnorrish <jnorrish@7437ebc0-4e4f-0410-a91c-e60b824a4b0e>
2010-09-08 14:58:57 +0000

    More work in the process of getting a non-working script to a working one.
    git-svn-id: file:///project/repositories/svn2/schenker/trunk@287 7437ebc0-4e4f-0410-a91c-e60b824a4b0e

--- scripts/eats/create_entities_from_profiles.py
+++ scripts/eats/create_entities_from_profiles.py
@@ -4,7 +4,8 @@
 specified directories."""
 
 import glob
-from os.path import abspath, join
+from os.path import abspath, dirname, join, split
+import re
 import sys
 
 from lxml import etree
@@ -13,14 +14,16 @@ from lxml import etree
 from eatsml.dispatcher import Dispatcher
 
 
-SERVER_URL = 'http://localhost/sdo/eats/'
+SERVER_URL = 'http://localhost/eats/'
 USERNAME = 'jamie'
-PASSWORD = ''
+PASSWORD = 'password'
 
 TEI_NAMESPACE = 'http://www.tei-c.org/ns/1.0'
 NSMAP = {'tei': TEI_NAMESPACE}
 
-GET_PROFILE_NAME = etree.XPath('tei:TEI/tei:teiHeader/tei:fileDesc/tei:titleStmt/tei:title[@type="file"]/tei:term', namespace=NSMAP)
+GET_PROFILE_NAME = etree.XPath('/tei:TEI/tei:teiHeader/tei:fileDesc/tei:titleStmt/tei:title[@type="file"]/tei:term', namespaces=NSMAP)
+
+MULTIPLE_WHITESPACE = re.compile(r'\s+')
 
 
 def main ():
@@ -30,8 +33,9 @@ def main ():
     dirs = sys.argv[1:]
     for directory in dirs:
         filename_pattern = abspath(join(directory, '*.xml'))
+        entity_type = split(abspath(directory))[1]
         for filename in glob.glob(filename_pattern):
-            process_profile(dispatcher, filename, eats_data, directory)
+            process_profile(dispatcher, filename, eats_data, entity_type)
 
 def get_eats_data (dispatcher):
     """Return a dictionary of EATS data to be used in the creation of
@@ -39,18 +43,42 @@ def get_eats_data (dispatcher):
     doc = dispatcher.get_base_document()
     eats_data = {}
     eats_data['authority'] = doc.get_default_authority()
-    eats_data['name_type'] = doc.get_name_types()[0]
     eats_data['entity_types'] = {}
     for entity_type in doc.get_entity_types():
         eats_data['entity_types'][unicode(entity_type)] = entity_type
+    eats_data['name_type'] = doc.get_name_types()[0]
+    eats_data['name_part_types'] = {}
+    for name_part_type in doc.get_name_part_types():
+        eats_data['name_part_types'][name_part_type.text] = name_part_type
+    eats_data['languages'] = {}
+    for language in doc.get_languages():
+        eats_data['languages'][language.name] = language
+    eats_data['scripts'] = {}
+    for script in doc.get_scripts():
+        eats_data['scripts'][script.name] = script
     return eats_data
     
 def process_profile (dispatcher, filename, eats_data, entity_type):
     """Create an EATS entity from the information in the profile at
     `filename`."""
     profile = etree.parse(filename)
-    profile_data = {}
-    name = GET_PROFILE_NAME(profile)[0]
+    profile_data = {
+        'entity_type': eats_data['entity_types'][entity_type],
+        'display_form': '', 'terms of address': '', 'given': '',
+        'family': '', 'language': eats_data['languages'].values()[0],
+        'script': eats_data['scripts'].values()[0],
+        }
+    name = GET_PROFILE_NAME(profile)[0].text
+    name = MULTIPLE_WHITESPACE.sub(' ', name)
+    print name
+    name = name.encode('utf-8')
+    if entity_type == 'person':
+        try:
+            profile_data['family'], profile_data['given'] = name.split(', ')
+        except:
+            profile_data['display_form'] = name
+    else:
+        profile_data['display_form'] = name
     doc = dispatcher.get_base_document_copy()
     authority_record = doc.create_authority_record(
         id='new_authority_record', authority=eats_data['authority'])
@@ -66,19 +94,27 @@ def process_profile (dispatcher, filename, eats_data, entity_type):
         'new_name_assertion', authority_record=authority_record,
         name_type=eats_data['name_type'], is_preferred=True)
     name.display_form = profile_data['display_form']
-    for name_part_type in ('terms_of_address', 'given_name', 'family_name'):
-        if eats_data[name_part_type]:
-            name.create_name_part(eats_data[name_part_type],
+    name.language = profile_data['language']
+    name.script = profile_data['script']
+    for name_part_type in ('terms of address', 'given', 'family'):
+        if profile_data.get(name_part_type):
+            name.create_name_part(eats_data['name_part_types'][name_part_type],
                                   profile_data[name_part_type])
     message_name = profile_data['display_form']
     if not message_name:
-        message_name = ' '.join([profile_data['given_name'],
-                                 profile_data['family_name']]).strip()
+        message_name = ' '.join([profile_data['given'],
+                                 profile_data['family']]).strip()
     message = 'Created new entity "%s" from lookup.' % message_name
+    print etree.tostring(doc)
     url = dispatcher.import_document(doc, message)
     eatsml = dispatcher.get_processed_import(url)
     entity = eatsml.get_entities()[0]
-
+    keys = [record.system_id for record in entity.get_default_authority_records()]
+    # Save the profile with a filename of its key.
+    new_filename = join(dirname(filename), keys[0])
+    new_file = open(new_filename, 'w')
+    profile.write(new_file, encoding='utf-8', pretty_print=True, xml_declaration=True)
+    new_file.close()
     
 
 if __name__ == '__main__':
