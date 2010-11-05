ee9e34bee1f58844832abd82d1e9ee33e5010135
jnorrish <jnorrish@7437ebc0-4e4f-0410-a91c-e60b824a4b0e>
2010-11-05 13:47:37 +0000

    Corrected paths.
    git-svn-id: file:///project/repositories/svn2/schenker/trunk@400 7437ebc0-4e4f-0410-a91c-e60b824a4b0e

--- scripts/eats/import-entities.sh
+++ scripts/eats/import-entities.sh
@@ -5,25 +5,25 @@
 
 python generate-csv-from-profiles.py place ../../xmod/webapp/root/xml/content/profiles/place/
 
-python generate-csv-from-profiles.py organization ../../trunk/xmod/webapp/root/xml/content/profiles/organization/
+python generate-csv-from-profiles.py organization ../../xmod/webapp/root/xml/content/profiles/organization/
 
 
 ##################################
 # Import entities from CSV files.
 
-python create_entities_from_csv.py person person.csv ../../trunk/xmod/webapp/root/xml/content/profiles/person/
+python create_entities_from_csv.py person person.csv ../../xmod/webapp/root/xml/content/profiles/person/
 
 # Any composer not found in EATS will be output on the command line.
-python create_entities_from_csv.py composition composition.csv ../../trunk/xmod/webapp/root/xml/content/profiles/
+python create_entities_from_csv.py composition composition.csv ../../xmod/webapp/root/xml/content/profiles/
 
 # Any author/editor (it's always Schenker) not found in EATS will be
 # output on the command line.
-python create_entities_from_csv.py work work.csv ../../trunk/xmod/webapp/root/xml/content/profiles/work/
+python create_entities_from_csv.py work work.csv ../../xmod/webapp/root/xml/content/profiles/work/
 
-python create_entities_from_csv.py term term.csv ../../trunk/xmod/webapp/root/xml/content/profiles/
+python create_entities_from_csv.py term term.csv ../../xmod/webapp/root/xml/content/profiles/
 
-python create_entities_from_csv.py place place.csv ../../trunk/xmod/webapp/root/xml/content/profiles/place/
+python create_entities_from_csv.py place place.csv ../../xmod/webapp/root/xml/content/profiles/place/
 
-python create_entities_from_csv.py organization organization.csv ../../trunk/xmod/webapp/root/xml/content/profiles/organization
+python create_entities_from_csv.py organization organization.csv ../../xmod/webapp/root/xml/content/profiles/organization/
 
 
