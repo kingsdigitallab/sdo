9cafae6e72ef9211c3b5476dd06f2a55841d6404
jvieira <jvieira@7437ebc0-4e4f-0410-a91c-e60b824a4b0e>
2011-05-19 14:13:17 +0000

    Fixed imports.
    git-svn-id: file:///project/repositories/svn2/schenker/trunk@1350 7437ebc0-4e4f-0410-a91c-e60b824a4b0e

--- django/sdo/apps/archives/forms.py
+++ django/sdo/apps/archives/forms.py
@@ -1,14 +1,10 @@
-from django.forms import *
+from archives.models import Document
+from django import forms
 from django.forms.extras.widgets import SelectDateWidget
-from django.utils.safestring import mark_safe
-
-from archives.models import *
-
 
 class DocumentForm(forms.ModelForm):
-   coverage_start = forms.DateField(label="Start Date",widget=SelectDateWidget(years=range(1879,1991)),help_text=u"Enter the start date for material covered in this document.")
-   coverage_end = forms.DateField(label="End Date", widget=SelectDateWidget(years=range(1879,1991)),help_text=u"For documents covering more than one day indicate the end date for material covered.")
-   
+    coverage_start = forms.DateField(label="Start Date",widget=SelectDateWidget(years=range(1879,1991)),help_text=u"Enter the start date for material covered in this document.")
+    coverage_end = forms.DateField(label="End Date", widget=SelectDateWidget(years=range(1879,1991)),help_text=u"For documents covering more than one day indicate the end date for material covered.")
      
-   class Meta:
+    class Meta:
         model = Document;
