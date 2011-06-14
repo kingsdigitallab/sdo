bba66a29f490ab4f1261068008a4fad58b11084a
jvieira <jvieira@7437ebc0-4e4f-0410-a91c-e60b824a4b0e>
2011-06-14 15:23:40 +0000

    Added tests to see which accordion elements are in the page.
    git-svn-id: file:///project/repositories/svn2/schenker/trunk@1676 7437ebc0-4e4f-0410-a91c-e60b824a4b0e

--- xmod/webapp/root/_a/s/config.js
+++ xmod/webapp/root/_a/s/config.js
@@ -1,57 +1,63 @@
 // xMod 1.3 JS Library: Configuration
-// This file can be edited 
+// This file can be edited
 
- $("document").ready( function() {
-    $(function () {
-            var tabContainers = $('div.tabs > div');
-            tabContainers.hide().filter(':first').show();
-            
-            $('div.tabs ul.tabNavigation a').click(function () {
-            tabContainers.hide();
-            tabContainers.filter(this.hash).show();
-            $('div.tabs ul.tabNavigation a').removeClass('selected');
-            $(this).addClass('selected');
-            return false;
-            }).filter(':first').click();
-            });
-   });
+$("document").ready(function () {
+  $(function () {
+    var tabContainers = $('div.tabs > div');
+    tabContainers.hide().filter(':first').show();
+    
+    $('div.tabs ul.tabNavigation a').click(function () {
+      tabContainers.hide();
+      tabContainers.filter(this.hash).show();
+      $('div.tabs ul.tabNavigation a').removeClass('selected');
+      $(this).addClass('selected');
+      return false;
+    }).filter(':first').click();
+  });
+});
 
 
 $("html").addClass("js");
-$.fn.accordion.defaults.container = false; 
-$(function() {
-  $("#acc3").accordion({initShow : "#current"});
-  $("#acc1").accordion({
-      el: ".h", 
-      head: "h4, h5", 
-      next: "div", 
-      initShow : "div.outer:eq(1)"
-  });
-  $("#acc2").accordion({
-      obj: "div", 
-      wrapper: "div", 
-      el: ".h", 
-      head: "h4, h5", 
-      next: "div", 
-      initShow : "div.shown"
+
+$(function () {
+  if ($('#acc1').length) {
+    $.fn.accordion.defaults.container = false;
+    $("#acc3").accordion({
+      initShow: "#current"
+    });
+    $("#acc1").accordion({
+      el: ".h",
+      head: "h4, h5",
+      next: "div",
+      initShow: "div.outer:eq(1)"
+    });
+    $("#acc2").accordion({
+      obj: "div",
+      wrapper: "div",
+      el: ".h",
+      head: "h4, h5",
+      next: "div",
+      initShow: "div.shown"
     });
     
     
-  $("html").removeClass("js");
-$(".erased2").css("display","none");
+    $("html").removeClass("js");
+    $(".erased2").css("display", "none");
+  }
 });
 
-    function show(a) {
-      
-if (a.childNodes[0].style.display == "inline") {
-a.childNodes[0].style.display = "none"
-} else {
-a.childNodes[0].style.display = "inline"
-}
+function show(a) {
+  if (a.childNodes[0].style.display == "inline") {
+    a.childNodes[0].style.display = "none"
+  } else {
+    a.childNodes[0].style.display = "inline"
+  }
 }
 
-$(function() {
-               $( "#accordion" ).accordion({ autoHeight: false });
-       
-               
-            });
+$(function () {
+  if ($('.accordion').length) {
+    $('.accordion').accordion({
+      autoHeight: false
+    });
+  }
+});
