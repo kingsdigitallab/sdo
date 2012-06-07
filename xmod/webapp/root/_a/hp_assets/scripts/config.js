c8de0fa6a469c5cceb250088531cf26ea312b317
pcaton <pcaton@7437ebc0-4e4f-0410-a91c-e60b824a4b0e>
2012-06-07 07:24:04 +0000

    added hp_assets dir
    git-svn-id: file:///project/repositories/svn2/schenker/trunk@2695 7437ebc0-4e4f-0410-a91c-e60b824a4b0e

---
+++ xmod/webapp/root/_a/hp_assets/scripts/config.js
@@ -0,0 +1,31 @@
+/*
+
+	Config.js: main javascript file
+
+*/
+
+$(document).ready( function() {
+
+	/* modules */
+	
+	/* link behaviours */
+	$("a.ctrl.tooltip").tooltip({
+		position: "bottom right",
+		predelay: 30,
+		delay: 50,
+		effect: "fade",
+		fadeInSpeed: 400,
+		fadeOutSpeed: 400,
+		tipClass: "tooltipBox"
+	});	
+	
+	if ( $.fancybox ) {
+		$(".ctrl.overlayTrigger").fancybox();
+	};
+	
+	/* tabs */
+	$(".mod.tabs ul.tabControls").tabs(".tabPanes > section");
+
+
+	
+});
