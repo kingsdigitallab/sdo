ede6b494b1b0b41e3f5f611c682fac0dde778881
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-11-05 12:38:58 +0000

    refactor(django): set subpath for the archives app
     To not conflict with the project base url

--- django/sdo/urls.py
+++ django/sdo/urls.py
@@ -1,8 +1,10 @@
+from kdl_ldap.signal_handlers import (
+    register_signal_handlers as kdl_ldap_register_signal_hadlers,
+)
+
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path, re_path
-from kdl_ldap.signal_handlers import \
-    register_signal_handlers as kdl_ldap_register_signal_hadlers
 
 kdl_ldap_register_signal_hadlers()
 
@@ -10,11 +12,10 @@ kdl_ldap_register_signal_hadlers()
 admin.autodiscover()
 
 urlpatterns = [
-    path('', include('archives.urls')),
-    path('admin/', admin.site.urls),
-    path('eats/', include('eats.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-
+    path("archives/", include("archives.urls")),
+    path("admin/", admin.site.urls),
+    path("eats/", include("eats.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
 
 # -----------------------------------------------------------------------------
@@ -23,8 +24,9 @@ urlpatterns = [
 try:
     if settings.DEBUG:
         import debug_toolbar
+
         urlpatterns = [
-            re_path(r'^__debug__/', include(debug_toolbar.urls)),
+            re_path(r"^__debug__/", include(debug_toolbar.urls)),
         ] + urlpatterns
 except ImportError:
     pass
@@ -33,10 +35,12 @@ except ImportError:
 # Static file DEBUGGING
 # -----------------------------------------------------------------------------
 if settings.DEBUG:
-    from django.conf.urls.static import static
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
     import os.path
 
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
     urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=os.path.join(settings.MEDIA_ROOT))
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=os.path.join(settings.MEDIA_ROOT)
+    )
